679d23f47eed2d7d80d459c601ffe40a02f6fc5e
Matthew Davis <github@mdavis.xyz>
2019-05-01 05:53:37 +0000

    remove trailing whitespace

--- lib/ansible/modules/files/file.py
+++ lib/ansible/modules/files/file.py
@@ -199,7 +199,7 @@ EXAMPLES = r'''
 '''
 RETURN = r'''
 path:
-    description: Equal to the path (or dest) variable passed as an argument. 
+    description: Equal to the path (or dest) variable passed as an argument.
     returned: state != link and state != hard
     type: str
     sample: /path/to/file.txt
@@ -212,15 +212,15 @@ diff:
     description: An object describing the state before and after the task.
     returned: sometimes
     type: dict
-    sample: 
-      after: 
+    sample:
+      after:
         path: file.txt
         state: absent
       before:
         path: file.txt
         state: file
 state:
-    description: 
+    description:
       - Equal to the nature of the path after the task is complete.
       - If the state argument is touch, the returned state value will be one of file, directory, link or absent.
       - If the state argument is not touch, the state returned on success equals the state argument.
